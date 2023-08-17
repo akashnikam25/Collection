2a29acecd0ec78b48f5a77d222150e19dd6cd503
akashnikam25 <akashvnikam@gmail.com>
2023-08-17 19:46:53 +0530

    circular queue implmentation added

--- queue/queue.go
+++ queue/queue.go
@@ -69,9 +69,8 @@ func main() {
 	// 	choice = enterChoice()
 	// 	fmt.Println("choice", choice)
 	// }
-
 	choice := 1 //by default 1 value we set it
-	q := &circularQueue{}
+	q := newCircularQueue(5)
 	for choice != 0 {
 		switch choice {
 		case 1:
@@ -95,22 +94,67 @@ type circularQueue struct {
 	arr   []int
 }
 
+func newCircularQueue(size int) *circularQueue {
+
+	if size == 0 {
+		return nil
+	}
+	return &circularQueue{
+		rear:  -1,
+		front: -1,
+		size:  size,
+		arr:   make([]int, size),
+	}
+}
+
+// Enqueue puts a element in the rear of queue
 func (q *circularQueue) enqueue(val int) {
 	if q.front == -1 {
 		q.rear = 0
 		q.front = 0
 		q.arr[q.rear] = val
-	} else if q.front == 0 && q.rear == q.size-1 {
+	} else if q.front != 0 && q.rear == q.size-1 {
+		q.rear = 0
+		q.arr[q.rear] = val
+	} else if (q.front == 0 && q.rear == q.size-1) || ((q.rear + 1 %q.size) == q.front) {
+		fmt.Println("queue is full")
+	} else {
 		q.rear++
 		q.arr[q.rear] = val
-	} else if q.front == 0 {
-
 	}
 }
+
+// dequeue removes element from front 
 func (q *circularQueue) dequeue() {
+	if q.front == -1 {
+		fmt.Println("queue is empty")
+		return
+	}
+	fmt.Println("deleted element is", q.arr[q.front])
+	if q.front == q.rear {
+		q.front, q.rear = -1, -1
+	} else if q.front == q.size-1 {
+		q.front = 0
+	} else {
+		q.front = q.front + 1
+	}
 
 }
 
+// prints the queue
 func (q *circularQueue) print() {
+	if q.front <= q.rear {
+		for i := q.front; i <= q.rear; i++ {
+			fmt.Print(q.arr[i], " ")
+		}
+	} else {
+		for i := q.front; i < q.size; i++ {
+			fmt.Print(q.arr[i], " ")
+		}
+		for i := 0; i <= q.rear; i++ {
+			fmt.Print(q.arr[i], " ")
+		}
 
+	}
+	fmt.Println()
 }
